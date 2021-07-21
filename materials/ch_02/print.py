b4d013626a0955c62ec9fec090048ab8c762a11c
epsilonxe <23317071+epsilonxe@users.noreply.github.com>
2021-07-21 11:46:47 +0700

    Added examples

---
+++ materials/ch_02/print.py
@@ -0,0 +1,14 @@
+print('Hello World')
+print('Hello', 'World')
+print('Hello', 'World', sep='++')
+print('Hello', 'World', '!', sep='++')
+print('My name is Ratthaprom', end='...')
+print('Promkam')
+print('Hahaha')
+print('----------------------------')
+x = 5
+y = 10
+s = 'Hello'
+print(x)
+print('We have x is', x)
+print('x + y = ', x, '+', y, '=', x+y)
